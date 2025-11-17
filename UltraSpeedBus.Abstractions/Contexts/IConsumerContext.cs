1715090da6004e2d6755f919d99a1eac082dd7fb
Yuri Melo <yurifullstack@gmail.com>
2025-11-17 20:54:46 -0300

    refactor: remove unused saga and transport interfaces

--- UltraSpeedBus.Abstractions/Contexts/IConsumerContext.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UltraSpeedBus.Abstractions.Contexts;
-
-public interface IConsumerContext : IMessageContext
-{
-    object Message { get; }
-    int DeliveryCount { get; }
-}
--- UltraSpeedBus.Abstractions/Transport/ITransportConsumer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UltraSpeedBus.Abstractions.Transport;
-
-public interface ITransportConsumer : IAsyncDisposable
-{
-    Task StartAsync(Func<ConsumerTransportContext, Task> onMessage, CancellationToken cancellationToken = default);
-    Task StopAsync(CancellationToken cancellationToken = default);
-}
