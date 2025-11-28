af9641cae7e7f41c663184f1f65031085ffa79f0
Yuri Melo <yurifullstack@gmail.com>
2025-11-27 21:57:47 -0300

    feat: enhance EditorConfig settings, fix method signature, and implement DynamicHandler for event handling

--- src/UltraSpeedBus/Mediator/UltraMediator.cs
+++ src/UltraSpeedBus/Mediator/UltraMediator.cs
@@ -3,157 +3,130 @@ using UltraSpeedBus.Abstractions.Mediator;
 
 namespace UltraSppedBus.Abstractions.Mediator;
 
-public class UltraMediator : IMediator
+/// <summary>
+/// The main mediator implementation.
+/// </summary>
+public partial class UltraMediator : IMediator
 {
-    private readonly Dictionary<Type, Func<object, Task<object>>> _commandHandlers = new();
-    private readonly Dictionary<Type, Func<object, Task<object>>> _queryHandlers = new();
-    private readonly Dictionary<Type, List<Func<object, Task>>> _eventHandlers = new();
-    private readonly Dictionary<Type, List<IDynamicHandler>> _dynamicHandlers = new();
+    private readonly Dictionary<Type, Func<object, Task<object>>> _commandHandlers = new ();
+    private readonly Dictionary<Type, Func<object, Task<object>>> _queryHandlers = new ();
+    private readonly Dictionary<Type, List<Func<object, Task>>> _eventHandlers = new ();
+    private readonly Dictionary<Type, List<IDynamicHandler>> _dynamicHandlers = new ();
 
-    #region Implement ISend
-    // Publisher 1 command x 1 Consumer
-    // Publisher 1 query   x 1 Consumer
     public Task<TResponse> SendAsync<TRequest, TResponse>(TRequest request)
     {
-        var type = typeof(TRequest);
+        Type type = typeof(TRequest);
 
-        if (_commandHandlers.TryGetValue(type, out var handler))
+        if (_commandHandlers.TryGetValue(type, out Func<object, Task<object>>? handler))
         {
             return InvokeHandler<TRequest, TResponse>(handler, request);
         }
 
-        if (_queryHandlers.TryGetValue(type, out var queryHandler))
+        if (_queryHandlers.TryGetValue(type, out Func<object, Task<object>>? queryHandler))
         {
             return InvokeHandler<TRequest, TResponse>(queryHandler, request);
         }
 
         throw new InvalidOperationException($"No handler registered for {type.Name}");
     }
-    #endregion
 
-    #region Implement IPublish
-    // Publisher 1 x Many Consumers
     public Task PublishAsync<TEvent>(TEvent message)
     {
-        if (message == null) throw new ArgumentException(nameof(message));
+        ArgumentNullException.ThrowIfNull(message);
 
-        var type = typeof(TEvent);
+        Type type = typeof(TEvent);
         var tasks = new List<Task>();
 
-        if (_eventHandlers.TryGetValue(type, out var eventHandlers))
+        if (_eventHandlers.TryGetValue(type, out List<Func<object, Task>>? eventHandlers))
         {
-            foreach (var handler in eventHandlers)
+            foreach (Func<object, Task> handler in eventHandlers)
+            {
                 tasks.Add(handler(message));
+            }
         }
 
         // You can disable this one
-        if (_dynamicHandlers.TryGetValue(type, out var dynamicEventHandlers))
+        if (_dynamicHandlers.TryGetValue(type, out List<IDynamicHandler>? dynamicEventHandlers))
         {
-            foreach (var handler in dynamicEventHandlers.OfType<DynamicHandler<TEvent>>())
+            foreach (DynamicHandler<TEvent> handler in dynamicEventHandlers.OfType<DynamicHandler<TEvent>>())
+            {
                 tasks.Add(handler.Handle(message));
+            }
         }
 
         return Task.WhenAll(tasks);
     }
-    #endregion
 
-    #region Implement IConsumerConnector
     public IHandlerHandle ConnectHandlerAsync<TMessage>(Func<ConsumeContext<TMessage>, Task> handler)
     {
         var dynamicHandler = new DynamicHandler<TMessage>(this, handler);
 
         lock (_dynamicHandlers)
         {
-            if (!_dynamicHandlers.TryGetValue(typeof(TMessage), out var list))
+            if (!_dynamicHandlers.TryGetValue(typeof(TMessage), out List<IDynamicHandler>? list))
             {
                 list = new List<IDynamicHandler>();
                 _dynamicHandlers.Add(typeof(TMessage), list);
             }
+
             list.Add(dynamicHandler);
         }
+
         return dynamicHandler;
     }
-    #endregion
 
-    #region Implement IConsumerRegister
     public void RegisterCommandHandler<TCommand, TResponse>(Func<CommandContext<TCommand>, Task<TResponse>> handler)
-    {
-        _commandHandlers[typeof(TCommand)] = async (object cmd) =>
+        => _commandHandlers[typeof(TCommand)] = async (object cmd) =>
         {
             var typed = (TCommand)cmd;
             var ctx = new CommandContext<TCommand>(typed);
-            var resp = await handler(ctx);
+            TResponse? resp = await handler(ctx);
             return resp!;
         };
-    }
 
     public void RegisterQueryHandler<TQuery, TResponse>(Func<QueryContext<TQuery>, Task<TResponse>> handler)
-    {
-        _queryHandlers[typeof(TQuery)] = async q =>
-        {
-            var typed = (TQuery)q;
-            var ctx = new QueryContext<TQuery>(typed);
-            var resp = await handler(ctx);
-            return resp!;
-        };
-    }
+        => _queryHandlers[typeof(TQuery)] = async q =>
+            {
+                var typed = (TQuery)q;
+                var ctx = new QueryContext<TQuery>(typed);
+                TResponse? resp = await handler(ctx);
+                return resp!;
+            };
 
     public void RegisterEventHandler<TEvent>(Func<EventContext<TEvent>, Task> handler)
     {
-        if (!_eventHandlers.TryGetValue(typeof(TEvent), out var list))
-            _eventHandlers[typeof(TEvent)] = list = new List<Func<object, Task>>();
-    }
-    #endregion
-
-    private async Task<TResponse> InvokeHandler<TRequest, TResponse>(
-        Func<object, Task<object>> handler,
-        TRequest request
-    )
-    {
-        if (request is null) throw new ArgumentNullException(nameof(request));
-
-        var response = await handler(request);
-        return (TResponse)response;
+        if (!_eventHandlers.TryGetValue(typeof(TEvent), out _))
+        {
+            _eventHandlers[typeof(TEvent)] = _ = new List<Func<object, Task>>();
+        }
     }
 
     internal void RemoveDynamicHandler(IDynamicHandler handler)
     {
         lock (_dynamicHandlers)
         {
-            if (_dynamicHandlers.TryGetValue(handler.MessageType, out var list))
+            if (_dynamicHandlers.TryGetValue(handler.MessageType, out List<IDynamicHandler>? list))
             {
                 list.Remove(handler);
 
                 if (list.Count == 0)
+                {
                     _dynamicHandlers.Remove(handler.MessageType);
+                }
             }
         }
     }
 
-    private class DynamicHandler<T> : IDynamicHandler
+    private static async Task<TResponse> InvokeHandler<TRequest, TResponse>(
+        Func<object, Task<object>> handler,
+        TRequest request)
     {
-        private readonly UltraMediator _mediator;
-        private readonly Func<ConsumeContext<T>, Task> _handler;
-
-        public DynamicHandler(
-            UltraMediator mediator,
-            Func<ConsumeContext<T>, Task> handler)
+        if (request is null)
         {
-            _mediator = mediator;
-            _handler = handler;
+            throw new ArgumentNullException(nameof(request));
         }
 
-        public Type MessageType => typeof(T);
-        // Pensa em uma interface para este método futuramente. Melhore o design
-        public Task Handle(object msg)
-        {
-            var ctx = new ConsumeContext<T>((T)msg);
-            return _handler(ctx);
-        }
-
-        public void Disconnect()
-        {
-            _mediator.RemoveDynamicHandler(this);
-        }
+        object response = await handler(request);
+        return (TResponse)response;
     }
 }
